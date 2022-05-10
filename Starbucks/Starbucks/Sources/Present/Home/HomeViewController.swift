28d86585004115979e9de5728f214ef26d58d30c
shingha <shingha1124@gmail.com>
2022-05-10 09:58:22 +0900

    [feature] home, pay viewcontroller 추가 및 테스트 코드 추가

--- Starbucks/Starbucks/Sources/Present/Home/HomeViewController.swift
+++ Starbucks/Starbucks/Sources/Present/Home/HomeViewController.swift
@@ -5,9 +5,32 @@
 //  Created by seongha shin on 2022/05/09.
 //
 
+import RxAppState
+import RxSwift
 import UIKit
 
 class HomeViewController: UIViewController {
+    private let viewModel: HomeViewModelProtocol
+    private let disposeBag = DisposeBag()
     
+    init(viewModel: HomeViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        bind()
+        layout()
+    }
     
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func bind() {
+        rx.viewDidLoad
+            .bind(to: viewModel.action().loadHomeData)
+            .disposed(by: disposeBag)
+    }
+    
+    private func layout() {
+    }
 }
--- Starbucks/Starbucks/Sources/Present/Home/HomeViewModel.swift
+++ Starbucks/Starbucks/Sources/Present/Home/HomeViewModel.swift
@@ -6,7 +6,29 @@
 //
 
 import Foundation
+import RxRelay
 
-class HomeViewModel {
+protocol HomeViewModelAction {
+    var loadHomeData: PublishRelay<Void> { get }
+}
+
+protocol HomeViewModelState {
+}
+
+protocol HomeViewModelBinding {
+    func action() -> HomeViewModelAction
+    func state() -> HomeViewModelState
+}
+
+typealias HomeViewModelProtocol = HomeViewModelBinding
+
+class HomeViewModel: HomeViewModelBinding, HomeViewModelAction, HomeViewModelState {
+    func action() -> HomeViewModelAction { self }
     
+    let loadHomeData = PublishRelay<Void>()
+    
+    func state() -> HomeViewModelState { self }
+    
+    init() {
+    }
 }
